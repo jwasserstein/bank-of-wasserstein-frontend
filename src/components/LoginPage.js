8598bcae39219869d9791fc0b6f6f44c9f5c18c3
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-15 01:28:32 +0000

    Rename project to bank-of-wasserstein

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {logIn, logOut} from '../store/actions/auth';
-import {Form, Input, Button, Segment} from 'semantic-ui-react';
+import {Form, Input, Button, Table} from 'semantic-ui-react';
 
 class LoginPage extends Component {
 	constructor(props){
@@ -25,21 +25,34 @@ class LoginPage extends Component {
 	
 	render() {
 		return (
-			<Segment style={{width: '50%', margin: '0 auto'}}>
-				<Form onSubmit={this.onSubmit}>
-					<Form.Field>
-						<label>Username:</label>
-						<input type='text' placeholder='username' name='username' value={this.state.username} onChange={this.onChange} />
-					</Form.Field>
+			<Table style={{width: '50%', margin: '0 auto'}}>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell textAlign='center'>
+							<h3>Log In</h3>
+						</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					<Table.Row>
+						<Table.Cell>
+							<Form onSubmit={this.onSubmit}>
+								<Form.Field>
+									<label>Username:</label>
+									<input type='text' placeholder='username' name='username' value={this.state.username} onChange={this.onChange} />
+								</Form.Field>
 
-					<Form.Field>
-						<label>Password:</label>
-						<input type='password' placeholder='password' name='password' value={this.state.password} onChange={this.onChange} />
-					</Form.Field>
+								<Form.Field>
+									<label>Password:</label>
+									<input type='password' placeholder='password' name='password' value={this.state.password} onChange={this.onChange} />
+								</Form.Field>
 
-					<Button type='submit'>Submit</Button>
-				</Form>
-			</Segment>
+								<Button type='submit'>Submit</Button>
+							</Form>
+						</Table.Cell>
+					</Table.Row>
+				</Table.Body>
+			</Table>
 		);
 	}
 }
