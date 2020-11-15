fa7d8288872893dda3e7a843cfb155060312aea1
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-15 15:50:07 +0000

    Make Sign Out link in navbar into a button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,17 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {logOut} from '../store/actions/auth';
-import {Menu} from 'semantic-ui-react';
+import {Menu, Button} from 'semantic-ui-react';
 
 const Banner = props => (
-	<Menu>
+	<Menu color='teal'>
 		<Menu.Item as={Link} to='/' header>Bank of Wasserstein</Menu.Item>
 		<Menu.Menu position='right'>
 				{props.authReducer.username ? 
 					[<Menu.Item as={Link} to='/' key='username'>{props.authReducer.username}</Menu.Item>,
 					<Menu.Item as={Link} to='/login' onClick={props.logOut} key='signout'>Sign Out</Menu.Item>] :
 					[<Menu.Item as={Link} to='/login' key='signin'>Sign In</Menu.Item>,
-					 <Menu.Item as={Link} to='/signup' key='signup'>Sign Up</Menu.Item>]}
+					 <Menu.Item as={Link} to='/signup' key='signup'><Button color='teal'>Sign Up</Button></Menu.Item>]}
 		</Menu.Menu>
 	</Menu>
 );
