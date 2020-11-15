4a847d33c69d416a882beba64c9b2af34e6e7b9b
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-15 12:30:18 +0000

    Add signUp action thunk and add check for falsy token to logIn action thunk.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,6 +9,30 @@ export function logIn(username, password) {
 				if(resp.error){ // refactor this to check for status code
 					return reject(resp.error);
 				}
+				if(!resp.token){
+					return reject('Error getting token');
+				}
+				localStorage.setItem('token', resp.token);
+				dispatch({type: LOG_IN, ...resp});
+				return resolve();
+			} catch(err) {
+				return reject(err.message);
+			}
+		});
+	}
+}
+
+export function signUp(username, email, password){
+	return dispatch => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const resp = await apiCall('post', 'https://testcontainer-sadjv2.run-us-west2.goorm.io/api/auth/signup', {username, email, password}, '');
+				if(resp.error){
+					return reject(resp.error);
+				}
+				if(!resp.token){
+					return reject('Error getting token');
+				}
 				localStorage.setItem('token', resp.token);
 				dispatch({type: LOG_IN, ...resp});
 				return resolve();
