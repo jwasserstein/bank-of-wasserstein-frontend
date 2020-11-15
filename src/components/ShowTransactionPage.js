be8900183aae05b7c1358dca71fcbcdfc5bbdd32
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-15 16:02:08 +0000

    Add back button to ShowTransactionPage

--- src/components/ShowTransactionPage.js
+++ src/components/ShowTransactionPage.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getTransactions} from '../store/actions/transactions';
-import {Segment, Statistic} from 'semantic-ui-react';
+import {Segment, Statistic, Button} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 import dayjs from 'dayjs';
 
 class ShowTransactionPage extends Component {
@@ -16,28 +17,31 @@ class ShowTransactionPage extends Component {
 		
 		if(transaction){
 			return (
-				<Segment compact style={{margin: '0 auto'}}>
-					<h3 style={{textAlign: 'center'}}>Transaction Details</h3>
-					<Statistic size='mini'>
-						<Statistic.Value>{dayjs(transaction.date).format('MM/DD/YYYY h:mA')}</Statistic.Value>
-						<Statistic.Label>Date</Statistic.Label>
-					</Statistic>
-					<Statistic size='mini'>
-						<Statistic.Value>${transaction.amount.toFixed(2)}</Statistic.Value>
-						<Statistic.Label>Amount</Statistic.Label>
-					</Statistic>
-					<Statistic size='mini'>
-						<Statistic.Value>${transaction.accountBalance.toFixed(2)}</Statistic.Value>
-						<Statistic.Label>Account Balance</Statistic.Label>
-					</Statistic>
-					
-					<h3 style={{textAlign: 'center'}}>Description</h3>
-					<div>{transaction.description}</div>
-					
-					<h3 style={{textAlign: 'center'}}>Receiving Account</h3>
-					<div><strong>Account Number</strong>: {transaction.receivingAccount}</div>
-					<div><strong>Routing Number</strong>: {transaction.receivingRouting}</div>
-				</Segment>
+				<div style={{textAlign: 'center'}}>
+					<Segment compact style={{margin: '0 auto'}}>
+						<h3 style={{textAlign: 'center'}}>Transaction Details</h3>
+						<Statistic size='mini'>
+							<Statistic.Value>{dayjs(transaction.date).format('MM/DD/YYYY h:mA')}</Statistic.Value>
+							<Statistic.Label>Date</Statistic.Label>
+						</Statistic>
+						<Statistic size='mini'>
+							<Statistic.Value>${transaction.amount.toFixed(2)}</Statistic.Value>
+							<Statistic.Label>Amount</Statistic.Label>
+						</Statistic>
+						<Statistic size='mini'>
+							<Statistic.Value>${transaction.accountBalance.toFixed(2)}</Statistic.Value>
+							<Statistic.Label>Account Balance</Statistic.Label>
+						</Statistic>
+
+						<h3 style={{textAlign: 'center'}}>Description</h3>
+						<div>{transaction.description}</div>
+
+						<h3 style={{textAlign: 'center'}}>Receiving Account</h3>
+						<div><strong>Account Number</strong>: {transaction.receivingAccount}</div>
+						<div><strong>Routing Number</strong>: {transaction.receivingRouting}</div>
+					</Segment>
+					<Link to='/' className='ui teal button' style={{marginTop: '10px'}}>Back</Link>					
+				</div>
 			);
 		} else {
 			return (
