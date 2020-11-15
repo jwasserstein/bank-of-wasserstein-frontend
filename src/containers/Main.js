9ffacfc6664f57255a2b37cf0b2b7555d4729837
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-15 01:59:46 +0000

    Add path for /signup to Main component

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,12 +3,14 @@ import LoginPage from '../components/LoginPage';
 import TransactionPage from '../containers/TransactionPage';
 import withAuth from '../hocs/withAuth';
 import ShowTransactionPage from '../components/ShowTransactionPage';
+import SignUp from '../components/SignUp';
 
 const Main = () => (
 	<div>
 		<Route exact path='/' component={withAuth(TransactionPage)} />
 		<Route exact path='/login' component={LoginPage} />
 		<Route exact path='/transactions/:transactionId' component={ShowTransactionPage} />
+		<Route exact path='/signup' component={SignUp} />
 	</div>
 );
 
