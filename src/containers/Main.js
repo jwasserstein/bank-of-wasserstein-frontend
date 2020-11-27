890de0084a7b77e5eb2a7c17be09ec741354d962
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-27 16:42:39 +0000

    Change SignUp.js from component to container.  Renamed SignUp.js to SignUpPage.js.  Update reference in Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,14 +3,14 @@ import LoginPage from './LoginPage';
 import TransactionPage from './TransactionPage';
 import withAuth from '../hocs/withAuth';
 import ShowTransactionPage from './ShowTransactionPage';
-import SignUp from '../components/SignUp';
+import SignUpPage from './SignUpPage';
 
 const Main = () => (
 	<div>
 		<Route exact path='/' component={withAuth(TransactionPage)} />
 		<Route exact path='/login' component={LoginPage} />
 		<Route exact path='/transactions/:transactionId' component={ShowTransactionPage} />
-		<Route exact path='/signup' component={SignUp} />
+		<Route exact path='/signup' component={SignUpPage} />
 	</div>
 );
 
