1c881c1800e6862a618bfd81d4dcc1097331874e
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-27 19:59:37 +0000

    Add timestamp for login and transaction updates to redux store.  Update placeholders and other logic in TransactionPage.js, Transaction.js, and Banner.js to use timestamp instead of checking for presence of transactions.

--- src/store/reducers/transactions.js
+++ src/store/reducers/transactions.js
@@ -7,7 +7,7 @@ const DEFAULT_STATE = {
 export function transactionReducer(state=DEFAULT_STATE, action){
 	switch(action.type){
 		case GET_TRANSACTIONS:
-			return {...state, transactions: action.transactions}
+			return {...state, transactions: action.transactions, lastUpdated: Date.now()}
 		case REMOVE_TRANSACTIONS:
 			return {...state, transactions: [], lastUpdated: 0}
 		default:
