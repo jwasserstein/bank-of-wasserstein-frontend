a87300161ef7ef2299a06d8d22aee3e00d0f0178
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-15 06:38:20 -0500

    Add name attribute to all form fields in SignupForm and LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,9 +4,9 @@ import './LoginForm.css';
 const LoginForm = props => (
     <form className='LoginForm-form' onSubmit={props.onSubmit}>
         <label htmlFor='username'>Username:</label>
-        <input type='text' className='LoginForm-field' value={props.username} onChange={props.onChange} name='username' placeholder='Username' autoComplete='off' required />
+        <input type='text' className='LoginForm-field' value={props.username} onChange={props.onChange} id='username' name='username' placeholder='Username' autoComplete='off' required />
         <label htmlFor='password'>Password:</label>
-        <input type='password' className='LoginForm-field' value={props.password} onChange={props.onChange} name='password' placeholder='Password' autoComplete='off' required />
+        <input type='password' className='LoginForm-field' value={props.password} onChange={props.onChange} id='password' name='password' placeholder='Password' autoComplete='off' required />
         <button type='submit' className='LoginForm-btn'>
             {props.loading ? 'Loading...' : 'Submit'}
         </button>
