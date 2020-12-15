17b1ce5449e796db5ba399c925bca25f52c0ce22
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-15 06:48:36 -0500

    Add autofocus attribute to first fields in LoginForm and SignupForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,7 @@ import './LoginForm.css';
 const LoginForm = props => (
     <form className='LoginForm-form' onSubmit={props.onSubmit}>
         <label htmlFor='username'>Username:</label>
-        <input type='text' className='LoginForm-field' value={props.username} onChange={props.onChange} id='username' name='username' placeholder='Username' autoComplete='off' required />
+        <input type='text' className='LoginForm-field' value={props.username} onChange={props.onChange} id='username' name='username' placeholder='Username' autoComplete='off' required autofocus />
         <label htmlFor='password'>Password:</label>
         <input type='password' className='LoginForm-field' value={props.password} onChange={props.onChange} id='password' name='password' placeholder='Password' autoComplete='off' required />
         <button type='submit' className='LoginForm-btn'>
