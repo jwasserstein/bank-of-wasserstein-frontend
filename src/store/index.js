87b6c6710bc4c8ccd4e25692cf4f8bb5af845edc
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-16 07:08:50 -0500

    Update index.js to only enable redux dev tools in a development environment

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,18 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 export function configureStore(){
-	return createStore(
-		rootReducer, 
-		compose(
+	let middleware;
+	if(process.env.NODE_ENV === 'development'){
+		middleware = compose(
 			applyMiddleware(thunk),
 			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
+		);
+	} else {
+		middleware = applyMiddleware(thunk);
+	}
+
+	return createStore(
+		rootReducer, 
+		middleware
 	);
 }
