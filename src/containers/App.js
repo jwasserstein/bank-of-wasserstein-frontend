0e8679875939a146f5700dd3866557fe942c9eeb
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-16 06:56:18 -0500

    Moved background-color from App.css to index.css.  Remove App.css

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,6 @@ import Main from './Main';
 import {configureStore} from '../store';
 import jwtdecode from 'jwt-decode';
 import {LOG_IN} from '../store/actionTypes';
-import './App.css';
 
 const store = configureStore();
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-// import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
 		<App />,
