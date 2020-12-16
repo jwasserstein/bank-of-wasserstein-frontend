5a0468ec167e174f1a536e9a92ae37cf6b368634
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-16 06:46:40 -0500

    Remove ShowTransactionPage and its route from Main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,7 +2,6 @@ import {Route, Switch} from 'react-router-dom';
 import LoginPage from './LoginPage';
 import TransactionPage from './TransactionPage';
 import withAuth from '../hocs/withAuth';
-import ShowTransactionPage from './ShowTransactionPage';
 import SignupPage from './SignupPage';
 import NewTransactionPage from './NewTransactionPage';
 import LandingPage from './LandingPage';
@@ -10,7 +9,6 @@ import LandingPage from './LandingPage';
 const Main = () => (
 	<Switch>
 		<Route path='/transactions/new' component={withAuth(NewTransactionPage)} />
-		<Route path='/transactions/:transactionId' component={withAuth(ShowTransactionPage)} />
 		<Route path='/signup' component={SignupPage} />
 		<Route path='/login' component={LoginPage} />
 		<Route path='/landing' component={LandingPage} />
