d115fd1ee60fd82dc50e19bafdf472c6380833c1
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-18 05:13:00 -0500

    Fix issue where a long transaction name collides with the amount and balance

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,14 +3,22 @@ import dayjs from 'dayjs';
 import './Transaction.css';
 
 const Transaction = props => (
-    <div className="TransactionPage-transaction">
-        <div className='TransactionPage-recipient'>
-            {props.description} 
-            <span className='TransactionPage-amount'>${props.amount.toFixed(2)}</span>
+    <div className='TransactionPage-transaction'>
+        <div>
+            <div className='TransactionPage-recipient'>
+                {props.description} 
+            </div>
+            <div className='TransactionPage-date'>
+                {dayjs(props.date).format('MM/DD/YYYY h:mmA')} 
+            </div>
         </div>
-        <div className='TransactionPage-date'>
-            {dayjs(props.date).format('MM/DD/YYYY h:mmA')} 
-            <span className='TransactionPage-balance'>${props.accountBalance.toFixed(2)}</span>
+        <div>
+            <div className='TransactionPage-amount'>
+                ${props.amount.toFixed(2)}
+            </div>
+            <div className='TransactionPage-balance'>
+                ${props.accountBalance.toFixed(2)}
+            </div>
         </div>
     </div>
 );
--- todo.md
+++ todo.md
@@ -1,9 +1,7 @@
-- Refactor LandingPage.js to fix height responsiveness.  main-container has hard-coded height: 700px.
-- Pull state out of Navbar.js, make each page stateful and pass everything down as props
-- Check that NewTransactionPage is centered
 - Organize container and component folders
 - Add pictures to placeholders on landing page
 - Fix coins-growing image squishing
 - Change links to make it clear you're going to the transaction list page
 - Refactor to use rem
 - Make everything smaller
+- Add accounts page
