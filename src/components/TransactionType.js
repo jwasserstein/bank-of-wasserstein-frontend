ae57c0a9bdaa56b73b9e4dc9f8e1271a1f5bf8eb
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-18 05:04:16 -0500

    Update TransactionType radio buttons so the label wraps with the radio button

--- src/components/TransactionType.js
+++ src/components/TransactionType.js
@@ -3,14 +3,23 @@ import './TransactionType.css';
 
 const TransactionType = props => (
     <div className='NewTransactionPage-radio-container'>
-        <input type='radio' id='deposit' className='NewTransactionPage-radio' value='deposit' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'deposit'}/>
-        <label htmlFor='deposit'>Deposit</label>
-        <input type='radio' id='withdrawal' className='NewTransactionPage-radio' value='withdrawal' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'withdrawal'}/>
-        <label htmlFor='withdrawal'>Withdrawal</label>
-        <input type='radio' id='transfer' className='NewTransactionPage-radio' value='transfer' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'transfer'}/>
-        <label htmlFor='transfer'>Transfer</label>
-        <input type='radio' id='generate' className='NewTransactionPage-radio' value='generate' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'generate'}/>
-        <label htmlFor='generate'>Generate</label>
+        <div>
+            <input type='radio' id='deposit' className='NewTransactionPage-radio' value='deposit' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'deposit'}/>
+            <label htmlFor='deposit'>Deposit</label>
+        </div>
+        <div>
+            <input type='radio' id='withdrawal' className='NewTransactionPage-radio' value='withdrawal' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'withdrawal'}/>
+            <label htmlFor='withdrawal'>Withdrawal</label>
+        </div>
+        
+        <div>
+            <input type='radio' id='transfer' className='NewTransactionPage-radio' value='transfer' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'transfer'}/>
+            <label htmlFor='transfer'>Transfer</label>
+        </div>
+        <div>
+            <input type='radio' id='generate' className='NewTransactionPage-radio' value='generate' name='transactionType' onChange={props.onChange} checked={props.transactionType === 'generate'}/>
+            <label htmlFor='generate'>Generate</label>
+        </div>
     </div>
 );
 
