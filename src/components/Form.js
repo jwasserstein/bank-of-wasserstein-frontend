6cb414f0c820eff76f6f7a01537616bef6f1fc4e
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-23 13:52:18 -0500

    Add autoCorrect='off' autoCapizatlize='none' to all input boxes

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,8 @@ const Form = props => {
     const fields = props.fields.map(f => (
         <label htmlFor={f.name} key={f.name} className='Form-label'>
             {f.label}:
-            <input type={f.type} className='Form-field' value={f.value} onChange={props.onChange} id={f.name} name={f.name} placeholder={f.label} autoComplete='off' required />
+            <input type={f.type} className='Form-field' value={f.value} onChange={props.onChange} id={f.name} 
+            name={f.name} placeholder={f.label} autoComplete='off' autoCorrect='off' autoCapitalize='none' required />
         </label>
     ));
 
--- src/components/NewTransactionForm.js
+++ src/components/NewTransactionForm.js
@@ -9,14 +9,14 @@ const NewTransactionForm = props => {
                 <label key='label' htmlFor='amount'>Amount:</label>,
                 <input key='input' type='number' className='NewTransactionForm-field' name='amount' 
                         id='amount' placeholder='Amount' min='0.01' step='0.01' value={props.amount} 
-                        onChange={props.onChange} autoComplete='off' required />
+                        onChange={props.onChange} autoComplete='off' autoCorrect='off' autoCapitalize='none' required />
             ])}
 
             {['transferAnotherUser'].includes(props.transactionType) && ([
                 <label key='label-counterparty' htmlFor='counterparty'>Counterparty:</label>,
                 <input key='input-counterparty' type='text' className='NewTransactionForm-field' name='counterparty' 
                         id='counterparty' placeholder='Counterparty' value={props.counterparty} 
-                        onChange={props.onChange} autoComplete='off' required />
+                        onChange={props.onChange} autoComplete='off' autoCorrect='off' autoCapitalize='none' required />
             ])}
             {['transferAnotherUser'].includes(props.transactionType) && ([
                 <label key='label-accountType' htmlFor='accountTypeAnotherUser'>Account Type:</label>,
@@ -66,7 +66,7 @@ const NewTransactionForm = props => {
                 <label key='label' htmlFor='number'>Number:</label>,
                 <input key='input' type='number' className='NewTransactionForm-field' name='number' 
                         id='number' placeholder='Number' min='1' step='1' value={props.number} 
-                        onChange={props.onChange} autoComplete='off' required />
+                        onChange={props.onChange} autoComplete='off' autoCorrect='off' autoCapitalize='none' required />
             ])}
 
             <Button form className='NewTransactionForm-form-btn'>
