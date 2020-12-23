dd81fd28e587d65e2c21d3fbcda59b7455b81c02
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-23 16:22:43 -0500

    Ensure Navbar closes when a link is clicked, and add a class for Navbar-spacer

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -9,6 +9,7 @@ class Navbar extends Component{
 		super(props);
 		this.state = {menuActive: ''};
 		this.toggleMenu = this.toggleMenu.bind(this);
+		this.closeMenu = this.closeMenu.bind(this);
 	}
 
 	toggleMenu(){
@@ -19,10 +20,14 @@ class Navbar extends Component{
 		}
 	}
 
+	closeMenu(){
+		this.setState({menuActive: ''});
+	}
+
 	render() {
 		return (
 			<div>
-				<div style={{height: '30px'}}></div>
+				<div className='Navbar-spacer'></div>
 				<header className={'Navbar ' + this.state.menuActive}>
 					<div className='Navbar-burger' onClick={this.toggleMenu}>
 						<span className='Navbar-bar'></span>
@@ -30,20 +35,20 @@ class Navbar extends Component{
 						<span className='Navbar-bar'></span>
 					</div>
 
-					<Link to='/' className='Navbar-brand'><h1>BANK OF WASSERSTEIN</h1></Link>
+					<Link onClick={this.closeMenu} to='/' className='Navbar-brand'><h1>BANK OF WASSERSTEIN</h1></Link>
 					<div className='Navbar-links-left'>
-						<Link to='/' className='Navbar-link'>Home</Link>
-						<Link to='/accounts' className='Navbar-link'>Accounts</Link>
-						<Link to='/about' className='Navbar-link'>About</Link>
+						<Link onClick={this.closeMenu} to='/' className='Navbar-link'>Home</Link>
+						<Link onClick={this.closeMenu} to='/accounts' className='Navbar-link'>Accounts</Link>
+						<Link onClick={this.closeMenu} to='/about' className='Navbar-link'>About</Link>
 					</div>
 					{this.props.username ?
 						<div className='Navbar-links-right'>
-							<Link to='/profile' className='Navbar-link'>{this.props.username}</Link>
-							<Link to='/login' className='Navbar-link Navbar-signup-btn' onClick={this.props.logOut}>Sign Out</Link>
+							<Link onClick={this.closeMenu} to='/profile' className='Navbar-link'>{this.props.username}</Link>
+							<Link onClick={this.closeMenu} to='/login' className='Navbar-link Navbar-signup-btn' onClick={this.props.logOut}>Sign Out</Link>
 						</div> : 
 						<div className='Navbar-links-right'>
-							<Link to='/login' className='Navbar-link'>Log In</Link>
-							<Link to='/signup' className='Navbar-link Navbar-signup-btn'>Sign Up</Link>
+							<Link onClick={this.closeMenu} to='/login' className='Navbar-link'>Log In</Link>
+							<Link onClick={this.closeMenu} to='/signup' className='Navbar-link Navbar-signup-btn'>Sign Up</Link>
 						</div>}
 				</header>
 			</div>
